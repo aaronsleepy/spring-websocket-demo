f8c8868513e50e0b6a147af867bdbe9343bd8071
aaronsleepy <aaron@kmong.com>
2022-05-08 18:49:53 +0900

    feature: Add WebSocketConfig

---
+++ src/main/java/com/socurites/demo/config/WebSocketConfig.java
@@ -0,0 +1,40 @@
+package com.socurites.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.MessageConverter;
+import org.springframework.messaging.handler.invocation.HandlerMethodArgumentResolver;
+import org.springframework.messaging.handler.invocation.HandlerMethodReturnValueHandler;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void configureMessageBroker(final MessageBrokerRegistry registry) {
+        // Enable a simple memory-based message broker
+        // to carry the greeting messages back to the client
+        // on destinations prefixed with /topic
+        registry.enableSimpleBroker("/topic");
+
+        // Designates the /app prefix for messages that are bound for methods
+        // annotated with @MessageMapping
+        // For example, /app/hello is the endpoint
+        // that the GreetingController.greeting() method is mapped to handle
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(final StompEndpointRegistry registry) {
+        // Registers the /demo-websocket endpoint,
+        // enabling SockJS fallback options so that alternate transports can be used
+        // if WebSocket is not available
+        registry.addEndpoint("/demo-websocket");
+    }
+}
